ec834361b437988d64f5ac736144899a331942df
S0ph1ia <badalovasophia@gmail.com>
2021-02-28 19:05:30 +0000

    changed op/ms to ops/s

--- index.py
+++ index.py
@@ -43,17 +43,17 @@ def benchmark():
         t1 = time.time()
         logger.info('Image Processing function ended, calculating metrics')
         if i >= 20:  # We let it warmup for first 20 rounds, then consider the last 20 metrics
-            throughput_time["Image"].append(1 / ((t1 - t0) * 1000))
-            average_duration_time["Image"].append(((t1 - t0) * 1000) / 1)
+            throughput_time["Image"].append(1 / (t1 - t0))  # in seconds
+            average_duration_time["Image"].append((t1 - t0) / 1)
 
-    # Printing out results for throughput
+            # Printing out results for throughput
     for name, numbers in throughput_time.items():
         logger.info("The throughput time")
         length = str(len(numbers))
         median = str(statistics.median(numbers))
         mean = str(statistics.mean(numbers))
         stdev = str(statistics.stdev(numbers))
-        output = "FUNCTION {} used {} times. > MEDIAN {} ops/ms > MEAN {} ops/ms  > STDEV {} ops/ms".format(name,
+        output = "FUNCTION {} used {} times. > MEDIAN {} ops/s > MEAN {} ops/s  > STDEV {} ops/s".format(name,
                                                                                                             length,
                                                                                                             median,
                                                                                                             mean,
@@ -67,7 +67,7 @@ def benchmark():
         median = str(statistics.median(numbers))
         mean = str(statistics.mean(numbers))
         stdev = str(statistics.stdev(numbers))
-        output = "FUNCTION {} used {} times. > MEDIAN {} ms/ops > MEAN {} ms/ops  > STDEV {} ms/ops".format(name,
+        output = "FUNCTION {} used {} times. > MEDIAN {} s/ops > MEAN {} s/ops  > STDEV {} s/ops".format(name,
                                                                                                             length,
                                                                                                             median,
                                                                                                             mean,
@@ -75,4 +75,3 @@ def benchmark():
         logger.info(output)
 
     logger.critical("The benchmark is finished properly")
-
